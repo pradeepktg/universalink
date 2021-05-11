7db97beaad53974a99d58e00a631ee8ec0529950
Pradeep <pradeep.ktg@gmail.com>
2021-05-11 22:49:20 +0530

    Added the well-known path

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/well-known')
 @app.route('/')
 def index():
     return "<h1>Welcome to Universal Link - iOS<h1>"
