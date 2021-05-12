8c61b1645a5dc30bf0cf2f8decd8fdbdb2c62339
Pradeep <pradeep.ktg@gmail.com>
2021-05-12 13:06:08 +0530

    opening the aasa file in browser

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
-from flask import Flask
-app = Flask(__name__,static_url_path='/well-known')
+from flask import Flask,url_for,send_from_directory, json
+import json
+
+app = Flask(__name__)
 @app.route('/')
+@app.route("/.well-known")
 def index():
     return "<h1>Welcome to Universal Link - iOS<h1>"
+
+@app.route("/.well-known/<path:filename>", methods=['GET','POST'])
+def wellknown(filename):
+    return send_from_directory(directory=app.root_path + '/well-known/',
+                                                      filename=filename,
+                                                      mimetype='application/json')
